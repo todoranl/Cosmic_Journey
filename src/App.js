d1c4526acfcabcf07df0c98be37db28fabda69b1
Todoran <uia19679@contiwan.com>
2023-12-12 03:57:22 +0200

    Implementing the header of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header"; // Verificați calea către fișierul Header
+import SolarSystemView from "./components/SolarSystemView";
+import PlanetDetail from "./components/PlanetDetail";
+import "./components/header.css"; // Verificați calea către fișierul CSS din directorul components
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<SolarSystemView />} />
+        <Route path="/planets/:planetName" element={<PlanetDetail />} />
+      </Routes>
+    </Router>
   );
 }
 

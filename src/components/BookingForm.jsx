19b9fcbf86dec9f1de25420c4def2c84ad2d5120
Todoran <uia19679@contiwan.com>
2023-12-13 06:43:23 +0200

    Implementing the booking part  of the project

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,72 +1,76 @@
 import React, { useState } from "react";
-import "./BookingForm.css";
+import "./BookingForm.css"; // Ensure this CSS file contains all the necessary styles.
 
 function BookingForm() {
   const [destination, setDestination] = useState("");
   const [passengers, setPassengers] = useState(1);
   const [departureDate, setDepartureDate] = useState("");
 
-  const handleDestinationChange = (e) => {
-    setDestination(e.target.value);
-  };
-
-  const handlePassengersChange = (e) => {
-    setPassengers(e.target.value);
-  };
-
-  const handleDepartureDateChange = (e) => {
-    setDepartureDate(e.target.value);
-  };
+  const planets = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Implement booking logic or API call here
     console.log(
       `Booking to ${destination} for ${passengers} passengers on ${departureDate}`
     );
+    // Here you would handle the submission of the form to your backend service.
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          Where:
-          <select value={destination} onChange={handleDestinationChange}>
-            <option value="">Select a planet</option>
-            <option value="Mercury">Mercury</option>
-            <option value="Venus">Venus</option>
-            <option value="Earth">Earth</option>
-            <option value="Mars">Mars</option>
-            <option value="Jupiter">Jupiter</option>
-            <option value="Saturn">Saturn</option>
-            <option value="Uranus">Uranus</option>
-            <option value="Neptune">Neptune</option>
-          </select>
-        </label>
+    <form onSubmit={handleSubmit} className="booking-form">
+      <div className="form-group">
+        <label htmlFor="destination">Destination:</label>
+        <select
+          id="destination"
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+          className="form-control"
+        >
+          <option value="">Select a planet</option>
+          {planets.map((planet) => (
+            <option key={planet} value={planet}>
+              {planet}
+            </option>
+          ))}
+        </select>
       </div>
-      <div>
-        <label>
-          Passengers:
-          <input
-            type="number"
-            value={passengers}
-            onChange={handlePassengersChange}
-            min="1"
-          />
-        </label>
+
+      <div className="form-group">
+        <label htmlFor="passengers">Passengers:</label>
+        <input
+          id="passengers"
+          type="number"
+          value={passengers}
+          onChange={(e) => setPassengers(e.target.value)}
+          min="1"
+          className="form-control"
+        />
       </div>
-      <div>
-        <label>
-          Departure:
-          <input
-            type="date"
-            value={departureDate}
-            onChange={handleDepartureDateChange}
-          />
-        </label>
+
+      <div className="form-group">
+        <label htmlFor="departureDate">Departure Date:</label>
+        <input
+          id="departureDate"
+          type="date"
+          value={departureDate}
+          onChange={(e) => setDepartureDate(e.target.value)}
+          className="form-control"
+        />
       </div>
-      <div>
-        <button type="submit">Book Now</button>
+
+      <div className="button-container">
+        <button type="submit" className="btn btn-red">
+          Book Now
+        </button>
       </div>
     </form>
   );

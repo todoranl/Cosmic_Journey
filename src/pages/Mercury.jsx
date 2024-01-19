425826cb6d6b74680d7b7001ce90856b738c4353
Savoiu Diana <patricia.patri997@gmail.com>
2024-01-19 19:40:28 +0200

    am terminat doamne ajuta

--- src/pages/Mercury.jsx
+++ src/pages/Mercury.jsx
@@ -1,14 +1,40 @@
-// src/pages/Mercur.jsx
 import React from 'react';
-import ItemThumbnailsDemo from '../components/ItemThumbnailsDemo';
+import '../styles/Planets.css'; // Import your CSS
+import BookingForm from '../components/BookingForm';
 
-const Mercur = () => {
-    return (
-        <div className="planet-page">
-            <h1>Mercur</h1>  {/* Updated page title */}
-            <ItemThumbnailsDemo planetName="Mercur" />  {/* Ensure this matches your image service */}
-            {/* Other content for Mercur */}
+const Mercury = () => {
+  return (
+    <>
+      <div className="mercury">
+        <div className="planets-text">
+        <h1>MERCURY</h1>
+          <p>Explore the wonders of the universe!</p>
+          
+          <div className="mercury-form">
+            <BookingForm />
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="mercury-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/mercuryfirstactivity.jpg?alt=media&token=d3a525b5-100e-41fa-a7c3-33df195bb7be" alt="Activitățile de pe Mercur" className="mercury-activities-image" />
+      </div>
+
+      <div className="mercury-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/mercurysecondactivity.jpg?alt=media&token=e6ab1a4b-5ab6-4617-b4ff-6b5da3499a83" alt="Activitățile de pe Mercur" className="mercury-activities-image" />
+      </div>
+
+      <div className="mercury-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/mercurythirdactivity.jpg?alt=media&token=eeeeeb4f-96c1-48a3-a4e6-5f05a3b59e5a" alt="Activitățile de pe Mercur" className="mercury-activities-image" />
+      </div>
+
+      
+      <div className="mercury-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/mercuryfourthactivity.jpg?alt=media&token=00821b93-a3b0-4bdb-badd-7ebdea9b5f11" alt="Activitățile de pe Mercur" className="mercury-activities-image" />
+      </div>
+
+    </> // Asigurați-vă că acesta este singurul div închizător pentru div-ul "mercury"
+  );
 };
-export default Mercur;
+
+export default Mercury;
--- src/pages/Venus.jsx
+++ src/pages/Venus.jsx
@@ -1,51 +1,40 @@
-// src/pages/Mercur.jsx
 import React from 'react';
-import ItemThumbnailsDemo from '../components/ItemThumbnailsDemo';
-import '../styles/Planets.css';
+import '../styles/Planets.css'; // Import your CSS
+import BookingForm from '../components/BookingForm';
 
 const Venus = () => {
-    const description = "Venus, often referred to as Earth’s sister planet due to their similar size, mass, proximity to the Sun, and bulk composition, presents a stark contrast to Earth in terms of surface conditions and atmosphere. Venus is the second planet from the Sun and the third brightest object in Earth's sky after the Sun and Moon. Named after the Roman goddess of love and beauty, Venus has been a subject of human interest since ancient times and is a significant feature in various cultural and religious practices."; // Your 500-word description
-    const specifications = {
-        distanceFromSun: "108.2 million km",
-        orbitalPeriod: "225 days",
-        surfaceTemperature: "462 °C",
-        radius: "6,051.8 km"
-    };
-    return (
-        <div className="planet-page">
-        
-            
-            <div className="planet-content">
-              
-                <ItemThumbnailsDemo planetName="Venus" />
-              
-                <p className="planet-description">{description}</p>
-            </div>
-           
-
-            <div className="planet-specification-tables">
-            <table> <tbody><tr><td>
-                    <thead><tr><th>Distance from Sun</th></tr></thead>
-                   {specifications.distanceFromSun}</td></tr></tbody>
-                </table>
-                <table>
-                <tbody><tr><td>
-                    <thead><tr><th>Orbital Period</th></tr></thead>
-                   {specifications.orbitalPeriod}</td></tr></tbody>
-                </table>
-                <table>
-                <tbody><tr><td>
-                    <thead><tr><th>Surface Temperature</th></tr></thead>
-                   {specifications.surfaceTemperature}</td></tr></tbody>
-                </table>
-                <table>
-                <tbody><tr><td>
-                    <thead><tr><th>Radius</th></tr></thead>
-                   {specifications.radius}</td></tr></tbody>
-                </table>
-            </div>
+  return (
+    <>
+      <div className="venus">
+        <div className="planets-text">
+        <h1>VENUS</h1>
+          <p>Explore the wonders of the universe!</p>
+          
+          <div className="venus-form">
+            <BookingForm />
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="venus-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/venusfirstactivity.jpg?alt=media&token=bd4ff0f2-44d6-4145-88ed-2d4beab8dd43" alt="Activitățile de pe Venus" className="venus-activities-image" />
+      </div>
+
+      <div className="venus-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/venussecondactivity.jpg?alt=media&token=850eccb9-4cf1-42fc-9fee-f5fa90877d9b" alt="Activitățile de pe Venus" className="venus-activities-image" />
+      </div>
+
+      <div className="venus-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/venusthirdactivity.jpg?alt=media&token=c81f63b5-13e0-4f05-8e26-05febecbc10e" alt="Activitățile de pe Venus" className="venus-activities-image" />
+      </div>
+
+      
+      <div className="venus-activities">
+        <img src="https://firebasestorage.googleapis.com/v0/b/cosmicjourney-dab3e.appspot.com/o/venusfourthactivity.jpg?alt=media&token=bb74fd4e-c6d4-4246-a532-0761114dc533" alt="Activitățile de pe Venus" className="venus-activities-image" />
+      </div>
+
+    </> // Asigurați-vă că acesta este singurul div închizător pentru div-ul "venus"
+  );
 };
 
 export default Venus;

d8189039f5020497c5bce9c853ce977f1ce0de97
Lucian Nicolaescu <ice.lucian97@gmail.com>
2024-01-26 11:03:52 +0000

    Add active color for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./header.css"; // Verify the path to the CSS file in the components directory
 
 const Header = () => {
@@ -25,14 +25,14 @@ const Header = () => {
     };
   }, []);
   const planetColors = {
-    Mercury: 'hover:text-gray-600',
-    Venus: 'hover:text-yellow-500',
-    Earth: 'hover:text-blue-500',
-    Mars: 'hover:text-red-500',
-    Jupiter: 'hover:text-orange-500',
-    Saturn: 'hover:text-pink-300',
-    Uranus: 'hover:text-green-500',
-    Neptune: 'hover:text-blue-700',
+    Mercury: ['hover:text-gray-600', '#4b5563'],
+    Venus: ['hover:text-yellow-500', '#eab308'],
+    Earth: ['hover:text-blue-500', '#3b82f6'],
+    Mars: ['hover:text-red-500', '#ef4444'],
+    Jupiter: ['hover:text-orange-500', '#f97316'],
+    Saturn: ['hover:text-pink-300', '#f9a8d4'],
+    Uranus: ['hover:text-green-500', '#22c55e'],
+    Neptune: ['hover:text-blue-700', '#1d4ed8'],
     // Add more planets and colors as needed
   };
 
@@ -50,22 +50,29 @@ const Header = () => {
   return (
     <header className={`header ${showHeader ? 'visible' : 'hidden'}`}>
       <div className="container">
-        <Link to={`/`} className="nav-link p-2 text-white hover:text-gray-300">
+        <NavLink to={`/`} className="nav-link p-2 text-white hover:text-gray-300">
           Cosmic Journey
-        </Link>
+        </NavLink>
         <nav className="flex space-x-4">
           {planets.map((planet) => (
-            <Link
+            <NavLink
               key={planet}
               to={`/planets/${planet.toLowerCase()}`}
-              className={`nav-link p-2 text-white ${planetColors[planet] || 'hover:text-gray-300'}`}
+              className={`nav-link p-2 text-white ${planetColors[planet][0] || 'hover:text-gray-300'}`}
+              style={({ isActive }) => {
+                return isActive ? { color: planetColors[planet][1] } : {};
+                }}
             >
               {planet}
-            </Link>
+            </NavLink>
           ))}
-           <Link to="/signin" className="nav-link p-2 text-white hover:text-purple-300">
+           <NavLink to="/signin" className="nav-link p-2 text-white hover:text-purple-300"
+           style={({ isActive }) => {
+            return isActive ? { color: '#d8b4fe' } : {};
+            }}
+            >
               Sign In
-            </Link>
+            </NavLink>
         </nav>
       </div>
     </header>
